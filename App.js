cfe37b399fd2f81d5b679cdde133f1619b0eae22
harshitbajpai87 <harshitbajpai8707@gmail.com>
2026-01-06 10:05:13 +0530

    Added navigation menu to CampuseX

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import Timetable from "./pages/Timetable";
 import Burnout from "./pages/Burnout";
 import Events from "./pages/Events";
 import LostFound from "./pages/LostFound";
 
 function App() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>CampuseX</h1>
+  const [page, setPage] = useState("timetable");
 
-      <Timetable />
-      <Burnout />
-      <Events />
-      <LostFound />
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>CampuseX</h1>
+      <p>Smart Campus Website</p>
+
+      {/* MENU */}
+      <div style={{ marginBottom: "20px" }}>
+        <button onClick={() => setPage("timetable")}>📅 Timetable</button>{" "}
+        <button onClick={() => setPage("burnout")}>🧠 Burnout</button>{" "}
+        <button onClick={() => setPage("events")}>📢 Events</button>{" "}
+        <button onClick={() => setPage("lost")}>🔍 Lost & Found</button>
+      </div>
+
+      <hr />
+
+      {/* PAGE CONTENT */}
+      {page === "timetable" && <Timetable />}
+      {page === "burnout" && <Burnout />}
+      {page === "events" && <Events />}
+      {page === "lost" && <LostFound />}
     </div>
   );
 }
